c4cb6c0253560b61b64bddd5e0c1a2bc3f7cdd12
Paul Holser <pholser@optimaldynamics.com>
2025-11-29 22:24:53 -0500

    more tests

---
+++ annogami/src/test/java/com/pholser/annogami/DirectOrIndirectPresenceOnParameterTest.java
@@ -0,0 +1,73 @@
+package com.pholser.annogami;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.List;
+
+import static com.pholser.annogami.Presences.DIRECT_OR_INDIRECT;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DirectOrIndirectPresenceOnParameterTest {
+  @Retention(RUNTIME) @interface A {
+    int value();
+  }
+
+  @Retention(RUNTIME) @interface Bs {
+    B[] value();
+  }
+
+  @Retention(RUNTIME) @Repeatable(Bs.class) @interface B {
+    int value();
+  }
+
+  static class ParameterHaver {
+    void p1(@A(1000) int x) {}
+    void p2(@B(2000) @B(3000) int x) {}
+    void p3(int x) {}
+  }
+
+  @Test void findsSingleNonRepeatable() throws Exception {
+    List<A> as = DIRECT_OR_INDIRECT.find(A.class, parameterOf("p1"));
+    assertThat(as).hasSize(1);
+
+    A a = as.stream().findFirst().orElseGet(Assertions::fail);
+    assertThat(a.value()).isEqualTo(1000);
+  }
+
+  @Test void missesNonDeclared() throws Exception {
+    List<A> as = DIRECT_OR_INDIRECT.find(A.class, parameterOf("p3"));
+
+    assertThat(as).isEmpty();
+  }
+
+  @Test void findsRepeatable() throws Exception {
+    List<B> bs = DIRECT_OR_INDIRECT.find(B.class, parameterOf("p2"));
+
+    assertThat(bs)
+      .extracting(B::value)
+      .containsExactlyInAnyOrder(2000, 3000);
+  }
+
+  @Test void findsContainerAnnotationOfRepeatable() throws Exception {
+    List<Bs> containers = DIRECT_OR_INDIRECT.find(Bs.class, parameterOf("p2"));
+    assertThat(containers).hasSize(1);
+
+    Bs bs = containers.stream().findFirst().orElseGet(Assertions::fail);
+    assertThat(bs.value())
+      .extracting(B::value)
+      .containsExactlyInAnyOrder(2000, 3000);
+  }
+
+  private static Parameter parameterOf(String methodName)
+    throws NoSuchMethodException {
+
+    Method m = ParameterHaver.class.getDeclaredMethod(methodName, int.class);
+    return m.getParameters()[0];
+  }
+}
---
+++ annogami/src/test/java/com/pholser/annogami/DirectOrIndirectPresenceOnRecordComponentTest.java
@@ -0,0 +1,68 @@
+package com.pholser.annogami;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.List;
+
+import static com.pholser.annogami.Presences.DIRECT_OR_INDIRECT;
+import static java.lang.annotation.ElementType.RECORD_COMPONENT;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DirectOrIndirectPresenceOnRecordComponentTest {
+  @Retention(RUNTIME) @Target(RECORD_COMPONENT) @interface A {
+    int value();
+  }
+
+  @Retention(RUNTIME) @Target(RECORD_COMPONENT) @interface Bs {
+    B[] value();
+  }
+
+  @Retention(RUNTIME) @Target(RECORD_COMPONENT) @Repeatable(Bs.class)
+  @interface B {
+    int value();
+  }
+
+  record R(@A(1) int a, @B(2) @B(3) int b, int c) {}
+
+  @Test void findsSingleNonRepeatable() {
+    List<A> as =
+      DIRECT_OR_INDIRECT.find(A.class, R.class.getRecordComponents()[0]);
+    assertThat(as).hasSize(1);
+
+    A a = as.stream().findFirst().orElseGet(Assertions::fail);
+    assertThat(a.value()).isEqualTo(1);
+  }
+
+  @Test void findsRepeatable() {
+    List<B> bs =
+      DIRECT_OR_INDIRECT.find(B.class, R.class.getRecordComponents()[1]);
+
+    assertThat(bs)
+      .extracting(B::value)
+      .containsExactlyInAnyOrder(2, 3);
+  }
+
+  @Test void findsContainerAnnotationOfRepeatable() {
+    List<Bs> containers =
+      DIRECT_OR_INDIRECT.find(Bs.class, R.class.getRecordComponents()[1]);
+    assertThat(containers).hasSize(1);
+
+    Bs bs = containers.stream().findFirst().orElseGet(Assertions::fail);
+
+    assertThat(bs.value())
+      .extracting(B::value)
+      .containsExactlyInAnyOrder(2, 3);
+  }
+
+  @Test void missesNonDeclared() {
+    List<A> as =
+      DIRECT_OR_INDIRECT.find(A.class, R.class.getRecordComponents()[2]);
+
+    assertThat(as).isEmpty();
+  }
+}
